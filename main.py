c98d6f0fb368915b383972a5049eaa2bd836d702
monk_mode <123338307+SaadARazzaq@users.noreply.github.com>
2023-08-20 23:56:21 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import rotatescreen, time
+
+rotate = rotatescreen
+
+primary = rotate.get_primary_display()
+
+start = primary.current_orientation
+
+for i in range(0, 15):
+    final_display = abs((start - i*180) % 360)
+    primary.rotate_to(final_display)
+    time.sleep(1)
