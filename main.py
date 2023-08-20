cc70d7da326195cf54e50d0fc72efc8961a53675
monk_mode <123338307+SaadARazzaq@users.noreply.github.com>
2023-08-20 23:58:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ primary = rotate.get_primary_display()
 start = primary.current_orientation
 
 for i in range(0, 15):
-    final_display = abs((start - i*180) % 360)
+    final_display = abs((start - i*90) % 360)
     primary.rotate_to(final_display)
     time.sleep(1)
